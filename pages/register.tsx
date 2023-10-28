26a92b71190fc31bbc170727a9cb2ece32e20a54
Dudu-Souza <eduardorsouza23@gmail.com>
2023-10-27 23:09:02 -0300

    Conexão com o backend na parte de registro, criação do toast e configuração das mensagens de erro - Registro

--- pages/register.tsx
+++ pages/register.tsx
@@ -3,8 +3,63 @@ import styles from "../styles/registerLogin.module.scss";
 import Head from "next/head";
 import { Container, Button, Form, FormGroup, Label, Input } from "reactstrap";
 import Footer from "@/src/components/common/footer";
+import { FormEvent, useState } from "react";
+import authService from "@/src/services/authService";
+import { useRouter } from "next/router";
+import ToastComponent from "@/src/components/common/toast";
 
 const Register = function () {
+	const router = useRouter();
+	const [toastIsOpen, setToastIsOpen] = useState(false);
+	const [toastMessage, setToastMessage] = useState("");
+
+	const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		const formData = new FormData(event.currentTarget)
+		const firstName = formData.get("firstName")!.toString()
+		const lastName = formData.get("lastName")!.toString()
+		const phone = formData.get("phone")!.toString()
+		const birth = formData.get("birth")!.toString()
+		const email = formData.get("email")!.toString()
+		const password = formData.get("password")!.toString()
+		const confirmPassword = formData.get("confirmPassword")!.toString()
+		const params = {
+			firstName,
+			lastName,
+			phone,
+			birth,
+			email,
+			password
+		};
+
+		if (password != confirmPassword) {
+			setToastIsOpen(true);
+
+			setTimeout(() => {
+				setToastIsOpen(false);
+			}, 1000 * 3);
+
+			setToastMessage("Senha e confirmação diferentes!");
+
+			return;
+		}
+
+		const { data, status } = await authService.register(params);
+
+		if (status === 201) {
+			router.push("/login?registred=true");
+		} else {
+			setToastIsOpen(true);
+
+			setTimeout(() => {
+				setToastIsOpen(false);
+			}, 1000 * 3);
+
+			setToastMessage(data.message);
+		}
+	};
+
 	return (
 		<>
 			<Head>
@@ -22,7 +77,7 @@ const Register = function () {
 					<p className={styles.formTitle}>
 						Bem-vindo(a) ao WebCode!
 					</p>
-					<Form className={styles.form}>
+					<Form className={styles.form} onSubmit={handleRegister}>
 						<p className="text-center">
 							<strong>Faça a sua conta!</strong>
 						</p>
@@ -130,14 +185,14 @@ const Register = function () {
 						</FormGroup>
 						<FormGroup>
 							<Label 
-								for="password"
+								for="confirmPassword"
 								className={styles.label}
 							>
 								CONFIRME SUA SENHA
 							</Label>
 							<Input 
-								id="password" 
-								name="password"
+								id="confirmPassword" 
+								name="confirmPassword"
 								type="password"
 								placeholder="Insira sua senha novamente"
 								required
@@ -152,6 +207,11 @@ const Register = function () {
 					</Form>
 				</Container>
 				<Footer />
+				<ToastComponent 
+					color="bg-danger" 
+					isOpen={toastIsOpen}
+					message={toastMessage}
+				/>
 			</main>
 		</>
 	);
