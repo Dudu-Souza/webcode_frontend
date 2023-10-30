359b1b1247afa2554c704c02415ba5910abbe1d4
Dudu-Souza <eduardorsouza23@gmail.com>
2023-10-29 21:33:14 -0300

    Criação e configuração da página de registro e login. Junto com suas conexões com o backend, e a criação da autenticação.

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -9,9 +9,14 @@ interface RegisterParams {
     password: string;
 };
 
+interface LoginParams {
+    email: string;
+    password: string;
+};
+
 const authService = {
     register: async (params: RegisterParams) => {
-        const res = await api.post("/auth/register", params).catch(error => {
+        const res = await api.post("/auth/register", params).catch((error) => {
             if (error.response.status === 400) {
                 return error.response;
             };
@@ -21,6 +26,22 @@ const authService = {
 
         return res;
     },
+
+    login: async (params: LoginParams) => {
+        const res = await api.post("/auth/login", params).catch((error) => {
+            if (error.response.status === 400 || error.response.status === 401) {
+                return error.response;
+            };
+
+            return error;
+        });
+
+        if (res.status === 200) {
+            sessionStorage.setItem("webcode-token", res.data.token);
+        }
+
+        return res;
+    }
 };
 
 export default authService;
